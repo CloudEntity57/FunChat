b48f1bfa78a79d03cb0a4cd2a85c522a4586fc88
Joshua Foster <joshua.a.foster@nasa.gov>
2020-04-26 15:34:26 -0500

    fixed user icon and added send message fn to ChatDBHub.cs

--- Hubs/ChatDBHub.cs
+++ Hubs/ChatDBHub.cs
@@ -15,5 +15,10 @@ namespace iCloset{
             var id = convId.ToString();
             await Groups.AddToGroupAsync(Context.ConnectionId, id);
         }
+
+        public Task SendMessageToRoom(string groupID, string message)
+        {
+            return Clients.Group(groupID).SendAsync("ReceiveMessage", message);
+        }
     }
 }
